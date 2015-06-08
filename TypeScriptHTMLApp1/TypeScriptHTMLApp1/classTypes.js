516f3a2f597c235780177dc73873780b2713a460
dominic wyler <dominic.wyler@gmail.com>
2015-06-08 17:18:26 +0200

    extended class types example with usages

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.js
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.js
@@ -1,6 +1,7 @@
 // explicit implementation
 var ExplicitAdaptable = (function () {
     function ExplicitAdaptable() {
+        this.name = "explicit";
     }
     ExplicitAdaptable.prototype.adapt = function () { return false; };
     return ExplicitAdaptable;
@@ -8,10 +9,19 @@ var ExplicitAdaptable = (function () {
 // implicit implementation
 var ImplicitAdaptable = (function () {
     function ImplicitAdaptable() {
+        this.name = "implicit";
     }
     ImplicitAdaptable.prototype.adapt = function () { return true; };
     return ImplicitAdaptable;
 })();
 var adaptable1 = new ExplicitAdaptable();
 var adaptable2 = new ImplicitAdaptable();
+function adapt(adaptable) {
+    adaptable.adapt();
+    return "adapted " + adaptable.name;
+}
+var result = adapt(adaptable1);
+result += "\n";
+result += adapt(adaptable2);
+document.body.innerHTML = result;
 //# sourceMappingURL=classTypes.js.map
--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.ts
@@ -1,16 +1,32 @@
 ﻿interface IAdaptable {
+    name: string
     adapt(): boolean;
 } 
 
 // explicit implementation
 class ExplicitAdaptable implements IAdaptable {
+    name: string;
     adapt() { return false; }
+    constructor() { this.name = "explicit"; }
 }
 
 // implicit implementation
 class ImplicitAdaptable {
+    name: string;
     adapt() { return true; }
+    constructor() { this.name = "implicit"; }
 }
 
 var adaptable1: IAdaptable = new ExplicitAdaptable();
 var adaptable2: IAdaptable = new ImplicitAdaptable();
+
+function adapt(adaptable: IAdaptable) {
+    adaptable.adapt();
+    return "adapted " + adaptable.name;
+}
+
+var result = adapt(adaptable1);
+result += "\n";
+result += adapt(adaptable2);
+
+document.body.innerHTML = result;
