dc893aed912cc90e8bf93955a5017c24513aa952
dominic wyler <dominic.wyler@gmail.com>
2015-06-10 09:11:53 +0200

    interface constructor definition and interface extension

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.js
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.js
@@ -1,3 +1,4 @@
+// ReSharper disable InconsistentNaming
 // explicit implementation
 var ExplicitAdaptable = (function () {
     function ExplicitAdaptable() {
@@ -24,4 +25,24 @@ var result = adapt(adaptable1);
 result += "\n";
 result += adapt(adaptable2);
 document.body.innerHTML = result;
+// error: Clock incorrectly implements IClockInterface
+/* class Clock implements IClockInterface {
+    constructor(hours: number, minutes: number, seconds: number) { }
+} */
+// this is because when a class implements an interface, only the instance
+// side of the class is checked, but the constructor is on the static side
+// of the class.
+var Clock = (function () {
+    function Clock(hours, minutes, seconds) {
+    }
+    return Clock;
+})();
+// need to work with the static part of the class
+var ClockStatic = Clock;
+var clock = new ClockStatic(8, 59, 12);
+var square = {}; // empty object of type Square
+square.x = 12;
+square.y = -7;
+square.length = 42;
+square.colour = "white";
 //# sourceMappingURL=classTypes.js.map
--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/classTypes.ts
@@ -1,4 +1,6 @@
-﻿interface IAdaptable {
+﻿// ReSharper disable InconsistentNaming
+
+interface IAdaptable {
     name: string
     adapt(): boolean;
 } 
@@ -30,3 +32,45 @@ result += "\n";
 result += adapt(adaptable2);
 
 document.body.innerHTML = result;
+
+// static / instance side of a class
+interface IClockInterface {
+    new (hours: number, minutes: number, seconds: number);
+}
+
+// error: Clock incorrectly implements IClockInterface
+/* class Clock implements IClockInterface {
+    constructor(hours: number, minutes: number, seconds: number) { }
+} */
+
+// this is because when a class implements an interface, only the instance
+// side of the class is checked, but the constructor is on the static side
+// of the class.
+
+class Clock {
+    constructor(hours: number, minutes: number, seconds: number) { }
+}
+
+// need to work with the static part of the class
+var ClockStatic: IClockInterface = Clock;
+var clock = new ClockStatic(8, 59, 12);
+
+// extending interfaces
+interface Shape {
+    x: number;
+    y: number;
+}
+
+interface Drawable {
+    colour: string;
+}
+
+interface Square extends Shape, Drawable {
+    length: number;
+}
+
+var square = <Square>{}; // empty object of type Square
+square.x = 12;
+square.y = -7;
+square.length = 42;
+square.colour = "white";
