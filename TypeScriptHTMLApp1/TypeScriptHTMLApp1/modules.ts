a94a6ed5587d5d83a08da0cad6fbcd497e5209f3
dominic wyler <dominic.wyler@gmail.com>
2015-06-10 12:01:43 +0200

    module basics

---
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/modules.ts
@@ -0,0 +1,23 @@
+﻿// ReSharper disable InconsistentNaming
+
+module Validation { 
+    export interface StringValidator {
+        validate(s: string): boolean;
+    }
+    export class ValidatorFactory {
+        static create() {
+            return new EvenLengthStringValidator();
+        }
+    }
+
+    // not visible outside of module
+    class EvenLengthStringValidator implements StringValidator {
+        validate(s: string) {
+            return s.length % 2 == 0;
+        }
+    }
+}
+
+var validator: Validation.StringValidator = Validation.ValidatorFactory.create();
+
+document.body.innerHTML = "string valid: " + validator.validate("stuff");
