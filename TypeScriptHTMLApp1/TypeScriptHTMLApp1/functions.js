74dd19b3f8043961ead2b132bee28e6123670d19
dominic wyler <dominic.wyler@gmail.com>
2015-06-11 14:01:02 +0200

    type of function with rest parameters

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.js
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.js
@@ -1,3 +1,4 @@
+// rest parameters
 function join(separator) {
     var names = [];
     for (var _i = 1; _i < arguments.length; _i++) {
@@ -6,4 +7,6 @@ function join(separator) {
     return names.join(separator);
 }
 var joined = join("o", "1");
+// type of the function with rest parameters
+var joinFunction = join;
 //# sourceMappingURL=functions.js.map
--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.ts
@@ -1,5 +1,9 @@
-﻿ function join(separator: string, ... names: string[]) {
-     return names.join(separator);
+﻿// rest parameters
+function join(separator: string, ...names: string[]) {
+    return names.join(separator);
 }
 
- var joined: string = join("o", "1");
+var joined: string = join("o", "1");
+
+// type of the function with rest parameters
+var joinFunction: (s: string, ...ns: string[]) => string = join;
