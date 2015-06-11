6215b748914f75c452e3f8d266e99b5fd9412dfa
dominic wyler <dominic.wyler@gmail.com>
2015-06-11 14:47:34 +0200

    type parameter: invocation without type inference

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/generics.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/generics.ts
@@ -3,7 +3,7 @@ function me<T>(arg: T): T {
     return arg;
 }
 
-var me1: number = me(2);
+var me1: number = me<number>(2);
 var me2: string[] = me(["hi", "sup"]);
 
 document.body.innerHTML = me1 + "<br/>";
