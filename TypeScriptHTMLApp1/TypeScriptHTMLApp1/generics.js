55f68296b188f1f839f0d04afac04adba281e1ca
dominic wyler <dominic.wyler@gmail.com>
2015-06-11 14:46:14 +0200

    type parameter

---
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/generics.js
@@ -0,0 +1,9 @@
+// type parameter
+function me(arg) {
+    return arg;
+}
+var me1 = me(2);
+var me2 = me(["hi", "sup"]);
+document.body.innerHTML = me1 + "<br/>";
+document.body.innerHTML += me2 + "<br/>";
+//# sourceMappingURL=generics.js.map
---
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/generics.ts
@@ -0,0 +1,10 @@
+﻿// type parameter
+function me<T>(arg: T): T {
+    return arg;
+}
+
+var me1: number = me(2);
+var me2: string[] = me(["hi", "sup"]);
+
+document.body.innerHTML = me1 + "<br/>";
+document.body.innerHTML += me2 + "<br/>";
