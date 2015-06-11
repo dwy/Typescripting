716e3c15ac721952d2746b46010c84e6adadc560
dominic wyler <dominic.wyler@gmail.com>
2015-06-11 14:15:06 +0200

    lambda capturing 'this' automatically

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.js
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.js
@@ -9,4 +9,23 @@ function join(separator) {
 var joined = join("o", "1");
 // type of the function with rest parameters
 var joinFunction = join;
+// lambda automatically capturing 'this'
+var context = {
+    contextValues: [42, 74, 89],
+    createValuator: function () {
+        var _this = this;
+        return function () {
+            return _this.contextValues.join("z");
+        };
+    }
+};
+// Note: createValuator() compiles as follows:
+// createValuator: function() {
+//    var _this = this;
+//    return function() {
+//        return _this.contextValues.join("z");
+//    };
+// }
+var valuator = context.createValuator();
+var valuated = valuator();
 //# sourceMappingURL=functions.js.map
--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/functions.ts
@@ -7,3 +7,25 @@ var joined: string = join("o", "1");
 
 // type of the function with rest parameters
 var joinFunction: (s: string, ...ns: string[]) => string = join;
+
+// lambda automatically capturing 'this'
+var context = {
+    contextValues: [42, 74, 89],
+    createValuator: function() {
+        return () => {
+            return this.contextValues.join("z");
+        }
+    }
+};
+
+// Note: createValuator() compiles as follows:
+// createValuator: function() {
+//    var _this = this;
+//    return function() {
+//        return _this.contextValues.join("z");
+//    };
+// }
+
+var valuator = context.createValuator();
+var valuated: string = valuator();
+
